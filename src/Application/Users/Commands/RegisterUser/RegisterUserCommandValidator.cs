3576c2242c5b5f04595e27540302e61df6c53d76
Dragos Rogojan <dragos.rogojan@gmail.com>
2020-09-15 19:18:06 +0300

    Existing username validator

---
+++ src/Application/Users/Commands/RegisterUser/RegisterUserCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using OpenChat.Application.Common.Interfaces;
+
+namespace OpenChat.Application.Users.Commands.RegisterUser
+{
+public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+{
+    private readonly IApplicationDbContext _dbContext;
+
+    public RegisterUserCommandValidator(IApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+
+        RuleFor(c => c.Username)
+            .MustAsync(BeUniqueUsername).WithMessage("Username already in use.");
+    }
+
+    private async Task<bool> BeUniqueUsername(string username, CancellationToken cancellationToken)
+    {
+        return await _dbContext
+            .Users
+            .AllAsync(u =>
+                u.Username != username, cancellationToken);
+    }
+}
+}
--- tests/WebAPI.AcceptanceTests/UsersApiTests.cs
+++ tests/WebAPI.AcceptanceTests/UsersApiTests.cs
@@ -1,11 +1,15 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using AspNetCore.Http.Extensions;
 using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using OpenChat.Domain.Entities;
 using WebAPI.AcceptanceTests.Models;
 using Xunit;
 using Xunit.Abstractions;
+using static WebAPI.AcceptanceTests.Testing;
 
 namespace WebAPI.AcceptanceTests
 {
@@ -35,5 +39,33 @@ namespace WebAPI.AcceptanceTests
             registeredUser.Username.Should().Be(user.Username);
             registeredUser.About.Should().Be(user.About);
         }
+
+[Fact]
+public async Task Should_Inform_When_A_Username_Is_Already_In_Use()
+{
+    var user = new
+    {
+        Username = "dragos.rogojan",
+        Password = "pass123",
+        About = "I already exist"
+    };
+
+    // backdoor manipulation
+    await AddAsync(new User
+    {
+        Id = Guid.NewGuid(),
+        Username = user.Username,
+        Password = user.Password,
+        About = user.Password
+    });
+
+    var response = await HttpClient.PostAsJsonAsync("api/users", user);
+
+    response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+    var errorDetails = await response.Content.ReadAsJsonAsync<ValidationProblemDetails>();
+
+    errorDetails.Errors["Username"].Should().Contain("Username already in use.");
+}
     }
 }
